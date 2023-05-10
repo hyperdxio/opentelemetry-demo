daf18c071b2d90804acf615f80acb00baa5e2be8
Warren <5959690+wrn14897@users.noreply.github.com>
2023-05-09 18:07:21 -0700

    feat: use @hyperdx/browser pkg

--- src/frontend/utils/telemetry/Instrumentation.js
+++ src/frontend/utils/telemetry/Instrumentation.js
@@ -1,34 +1,42 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-const opentelemetry = require('@opentelemetry/sdk-node');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
-const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-grpc');
-const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
-const { alibabaCloudEcsDetector } = require('@opentelemetry/resource-detector-alibaba-cloud');
-const { awsEc2Detector, awsEksDetector } = require('@opentelemetry/resource-detector-aws');
-const { containerDetector } = require('@opentelemetry/resource-detector-container');
-const { gcpDetector } = require('@opentelemetry/resource-detector-gcp');
-const { envDetector, hostDetector, osDetector, processDetector } = require('@opentelemetry/resources');
+// const opentelemetry = require('@opentelemetry/sdk-node');
+// const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+// const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
+// const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-grpc');
+// const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
+// const { alibabaCloudEcsDetector } = require('@opentelemetry/resource-detector-alibaba-cloud');
+// const { awsEc2Detector, awsEksDetector } = require('@opentelemetry/resource-detector-aws');
+// const { containerDetector } = require('@opentelemetry/resource-detector-container');
+// const { gcpDetector } = require('@opentelemetry/resource-detector-gcp');
+// const { envDetector, hostDetector, osDetector, processDetector } = require('@opentelemetry/resources');
 
-const sdk = new opentelemetry.NodeSDK({
-    traceExporter: new OTLPTraceExporter(),
-    instrumentations: [getNodeAutoInstrumentations()],
-    metricReader: new PeriodicExportingMetricReader({
-        exporter: new OTLPMetricExporter(),
-    }),
-    resourceDetectors: [
-        containerDetector,
-        envDetector,
-        hostDetector,
-        osDetector,
-        processDetector,
-        alibabaCloudEcsDetector,
-        awsEksDetector,
-        awsEc2Detector,
-        gcpDetector,
-    ],
+// const sdk = new opentelemetry.NodeSDK({
+//     traceExporter: new OTLPTraceExporter(),
+//     instrumentations: [getNodeAutoInstrumentations()],
+//     metricReader: new PeriodicExportingMetricReader({
+//         exporter: new OTLPMetricExporter(),
+//     }),
+//     resourceDetectors: [
+//         containerDetector,
+//         envDetector,
+//         hostDetector,
+//         osDetector,
+//         processDetector,
+//         alibabaCloudEcsDetector,
+//         awsEksDetector,
+//         awsEc2Detector,
+//         gcpDetector,
+//     ],
+// });
+
+// sdk.start();
+
+const HyperDX = require('@hyperdx/browser');
+
+HyperDX.init({
+  apiKey: '79d68bff-9496-4641-ad3c-bce74f47fa2a',
+  service: 'frontend',
+  tracePropagationTargets: [/api/i], // Set to link traces from frontend to backend requests
 });
-
-sdk.start();
